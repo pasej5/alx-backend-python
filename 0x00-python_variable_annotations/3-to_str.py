72cb1a831297e6b08d8a1d881f43f68af300834a
Jealous Matsikachando <pasej5@gmail.com>
2024-01-11 23:12:27 +0200

    basic anotations to string

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""
+type-annotated function to_str that takes a float n as argument
+and returns the string
+"""
+
+
+def to_str(n: float) -> str:
+    """type-annotated function to_str"""
+    return str(n)
