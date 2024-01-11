edc05ad79ffc5b72228b9041f3438e664283ad15
Jealous Matsikachando <pasej5@gmail.com>
2024-01-11 23:04:02 +0200

    type-annotated function concat that takes a string str1 and a string str2

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+'''type-annotated function concat that takes a
+string str1 and a string str2
+'''
+
+
+def concat(str1: str, str2: str) -> str:
+    """"""
+    concatStr = str1 + str2
+    return concatStr
