f768098472594ffd57ab0823bb747464ec13515b
Jealous Matsikachando <pasej5@gmail.com>
2024-01-11 22:35:33 +0200

    Basic annotations - floor

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""
+Type annoted function floor which taks a as argurment
+"""
+
+def floor(n: float) -> float:
+    """Function floo which takes a float as an arguement.
+    """
+    return (n)
