4f41ab2652725e8264588da6ebe645ca30644f39
Jealous Matsikachando <pasej5@gmail.com>
2024-01-12 03:26:33 +0200

    type-annotated function to_kv that takes a string k and an int OR float

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -1,22 +1,11 @@
-#!/u
-# r/bin/env python3
-"""
- type-annotated function to_kv that takes a string
- k and an int OR float
-"""
+#!/usr/bin/env python3
+'''Task 7's module.
+'''
+from typing import Union, Tuple
 
 
-from typing import Tuple
-
-
-def to_kv(k: str, v: any) -> Tuple[str, any]:
-    """Converts key-value pair to a tuple.
-
-    Args:
-        k (str): The key.
-        v (any): The value.
-
-    Returns:
-        Tuple[str, any]: A tuple containing the key and value.
-    """
-    return (k, v)
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
+    '''Converts a key and its value to a tuple of the key and
+    the square of its value.
+    '''
+    return (k, float(v**2))
