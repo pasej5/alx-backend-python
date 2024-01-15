828960f8b45796083cde26e704bff3b13d2675c4
Jealous Matsikachando <pasej5@gmail.com>
2024-01-15 12:35:04 +0200

    pycodestyle

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -7,6 +7,7 @@ Asyncronous that takes an integeras an argument
 import asyncio
 import random
 
+
 async def wait_random(max_delay: int = 10) -> float:
     """
     Asynchronous coroutine that generates a random delay between
